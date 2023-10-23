de33e8335ef0afc20a8bc4fc2c919621e18639b1
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-10-23 16:59:16 +0530

    Feat: Use Graph configuration context for the entire app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
+import App from './App';
+import { GraphConfigProvider } from './helpers/GraphConfigProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GraphConfigProvider>
         <App />
+      </GraphConfigProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
