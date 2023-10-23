6657fc9c791c1edbf0dbc84e2a1cad46b850839e
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-10-23 16:40:11 +0530

    Check: Loacation coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Box, BoxProps, Divider, styled } from '@mui/material';
+import useGeolocation from './hooks/useGeolocation';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+interface WeatherDisplayProps extends BoxProps {
+  children?: React.ReactNode;
+};
 
-export default App;
+const App: React.FC<WeatherDisplayProps> = (props) => {
+  const location = useGeolocation();
+  return(
+    <Box {...props}>
+      <Box>
+        <h1>Weather Display</h1>
+      </Box>
+      <Divider />
+      <Box>
+        <h2>Location</h2>
+        <p>{JSON.stringify(location)}</p>
+      </Box>
+    </Box>
+  )
+};
+
+export default styled(App)(({ theme }) => ({
+  height: '100%',
+  width: '100%',
+  backgroundColor: theme.palette.background.default,
+}));
